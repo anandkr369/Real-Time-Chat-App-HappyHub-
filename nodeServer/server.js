ef1ea3733ea7bc621166c36baf2cfed7d0d56a3e
Anand Kumar <anandkr369@gmail.com>
2024-01-03 07:05:08 +0530

    pushing all files removing modules

---
+++ nodeServer/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Your routes and other middleware...
+
+app.listen(8000, () => {
+  console.log('Server is running on port 8000');
+});
